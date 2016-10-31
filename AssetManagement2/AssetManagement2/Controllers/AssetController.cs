d17df21607eb92ed5aca9d71dcb65d43cbc475df
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-10-31 09:47:29 +0200

    Lisätty yksinkertainen toteutus tallennus paikoista

---
+++ AssetManagement2/AssetManagement2/Controllers/AssetController.cs
@@ -0,0 +1,152 @@
+﻿using AssetManagementWeb.Models;
+using AssetManagementWeb.Utilities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AssetManagementWeb.Database;
+using AssetManagement2.Models;
+using AssetManagement2.Database;
+
+namespace AssetManagementWeb.Controllers
+{
+    public class AssetController : Controller
+    {
+        // GET: Asset
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+
+
+        // GET: Asset/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asset/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult AssignLocations()
+        {
+            string json = Request.InputStream.ReadToEnd();
+            AssignLocationModel inputData =
+                JsonConvert.DeserializeObject<AssignLocationModel>(json);
+
+            bool success = false;
+            string error = "";
+
+            AssetLocationEntities entities = new AssetLocationEntities();
+            try
+            {
+                // Haetaan ensin paikan id-numero koodin perusteella.
+                int locationId = (from l in entities.AssetLocations
+                                  where l.Code == inputData.LocationCode
+                                  select l.Id).FirstOrDefault();
+
+                // Haetaan laitteen id-numero koodin perusteella.
+                int assetId = (from a in entities.Assets
+                               where a.Code == inputData.AssetCode
+                               select a.Id).FirstOrDefault();
+
+                if ((locationId > 0) && (assetId > 0))
+                {
+                    // Tallennetaan uusi rivi aikaleiman kanssa kantaan
+                    Assetlocation1 newEntry = new Assetlocation1();
+                    newEntry.LocationId = locationId;
+                    newEntry.AssetId = assetId;
+                    newEntry.LastSeen = DateTime.Now;
+
+                    entities.Assetlocations1.Add(newEntry);
+                    entities.SaveChanges();
+
+                    success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                error = error.GetType().Name + ": " + ex.Message;
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            //Palautetaan JSON-muotoinen tulos kutsujalle
+            var result = new { success = success, error = error };
+            return Json(result);
+        }
+        // POST: Asset/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: Asset/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AssetManagement2/AssetManagement2/Utilities/WebUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagementWeb.Utilities
+{
+    public static class WebUtilities
+    {
+        public static string ReadToEnd(this Stream stream)
+        {
+            int lenght = (int)stream.Length;
+            byte[] buffer = new byte[lenght];
+            int bytesRead = stream.Read(buffer, 0, lenght);
+            string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            return data;
+        }
+    }
+}
